4cf7f52d77cda7d9db46a0b654cd618ea537b6dc
SeoJoon Young <jmseo2@wirelessprvnat-172-16-204-65.near.illinois.edu>
2014-03-27 17:52:48 -0500

    add command line interface for chatting

--- chat_process/main.py
+++ chat_process/main.py
@@ -1,5 +1,7 @@
 import socket
 import config
+import threading
+import sys
 
 incoming_sockets = [None] * len(config.config['hosts'])
 
@@ -14,6 +16,7 @@ def init_sockets():
         out_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         out_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         out_sock.bind((ip, port))
+        out_sock.settimeout(0.01)
         incoming_sockets[index] = out_sock
 
 def unicast_send(destination, message):
@@ -40,9 +43,38 @@ def deliver(source):
         return: incoming message from the source process '''
     return unicast_receive(source)
 
+def process_message_out():
+    for line in sys.stdin:
+        multicast(line)
+
+
+def process_message_in():
+    while True:
+        for id, host in enumerate(config.config['hosts']):
+            try:
+                message = deliver(id)
+                print(id, message)
+            except socket.timeout:
+                pass
+            except BlockingIOError:
+                pass
 
 def run():
     init_sockets()
+
+    # multithreading to process sending/receiving messages
+    out_thread = threading.Thread(target=process_message_out)
+    out_thread.daemon = True
+
+    in_thread = threading.Thread(target=process_message_in)
+    in_thread.daemon = True
+
+    out_thread.start()
+    in_thread.start()
+
+    out_thread.join()
+    in_thread.join()
+
 
 if __name__ == '__main__':
     run()
