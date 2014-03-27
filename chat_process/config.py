8d67afbba40c0a69ca2f75f6f268263b8c357af4
SeoJoon Young <jmseo2@wirelessprvnat-172-16-204-65.near.illinois.edu>
2014-03-27 18:22:38 -0500

    ip address change for config file

--- chat_process/config.py
+++ chat_process/config.py
@@ -4,8 +4,8 @@ hosts is a list of (IP address, port, process id)
 
 config = {
     'hosts': [
-        ("localhost", 6401),
-        ("localhost", 6402),
+        ("172.16.150.36", 6401),
+        ("172.16.204.65", 6402),
         ("localhost", 6403),
     ],
 }
