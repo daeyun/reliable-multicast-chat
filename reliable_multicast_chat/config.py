50d3a0a091b85a32e333e2e83e4750574dd77a20
Daeyun Shin <daeyunshin@gmail.com>
2014-04-01 14:56:37 -0500

    Clean up config.py

--- reliable_multicast_chat/config.py
+++ reliable_multicast_chat/config.py
@@ -4,12 +4,8 @@ hosts is a list of (IP address, port)
 
 config = {
     'hosts': [
-        ("localhost", 16405),
-        ("localhost", 16406),
-        ("localhost", 16407),
-        # ("172.16.196.80", 16405),
-        # ("172.16.196.80", 16406),
-        # ("172.16.204.65", 16407),
-        # ("172.16.204.65", 16408),
+        ("localhost", 16400),
+        ("localhost", 16401),
+        ("localhost", 16402),
     ],
 }
