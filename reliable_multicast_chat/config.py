cd3f30ffc973691f04d4eace05ec212ca711c65a
SeoJoon Young <jmseo2@wirelessprvnat-172-16-238-254.near.illinois.edu>
2014-04-03 16:17:40 -0500

    add ordering parameter to config file

--- reliable_multicast_chat/config.py
+++ reliable_multicast_chat/config.py
@@ -1,11 +1,14 @@
 """
 hosts is a list of (IP address, port)
+
+ordering can be either 'casual' or 'total'
 """
 
 config = {
     'hosts': [
-        ("localhost", 16400),
-        ("localhost", 16401),
-        ("localhost", 16402),
+        ('localhost', 16400),
+        ('localhost', 16401),
+        ('localhost', 16402),
     ],
+    'ordering': 'casual',
 }
